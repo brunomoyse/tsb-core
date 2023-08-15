f18f04c4262a1bb953251e79b466c3740e22dba9
brunomoyse <moyse94@gmail.com>
2023-08-15 22:42:35 +0200

    Add custom Tailwind palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,15 @@ const config: Config = {
       backgroundImage: {
         'hero-pattern': "url('/main-bg.jpg')",
       },
-      backgroundColor: {
-        'egg-shell': '#FAF3E0',
-      }
+      colors: {
+        'charcoal': '#302b2c',
+        'coralPink': {
+          DEFAULT: '#e96c74',
+          darker: '#d25660' // Darkened version of coralPink.
+        },
+        'offWhite': '#f5f0f0',
+        'softPink': '#e9b3bc'
+      },
     },
   },
   plugins: [],
