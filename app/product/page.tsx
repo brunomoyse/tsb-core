64e34b43642a29e26de2443c3d5417bfae66eb21
brunomoyse <moyse94@gmail.com>
2023-08-15 22:44:38 +0200

    Load all products

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,13 +1,18 @@
+// app/product
+
+import dynamic from "next/dynamic";
 import { getClient } from "@/lib/client";
-import ProductCard from "@/components/ProductCard";
-import CartSidebar from "@/components/CartSidebar"
+
+const CartSidebar = dynamic(() => import('@/components/CartSidebar'), { ssr: false });
 import { PRODUCTS_QUERY } from "@/graphql/queries";
+import ProductCard from "@/components/ProductCard";
+
 
 export default async function Page() {
     const { data } = await getClient().query({
         query: PRODUCTS_QUERY,
         variables: {
-            first: 9,
+            first: -1,
             page: 1,
             lang: 'FR',
             tags: ["99df9038-0885-4043-9f36-9fdc18de167f"],
