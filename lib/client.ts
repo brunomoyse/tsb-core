eb578bd3bd1e0afb57b7bca363b6aed0f702e38f
brunomoyse <moyse94@gmail.com>
2023-08-26 21:39:04 +0200

    Update apollo use

---
+++ lib/client.ts
@@ -0,0 +1,16 @@
+// client.js
+
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+const GRAPHQL_URL = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+
+const httpLink = new HttpLink({
+    uri: GRAPHQL_URL,
+});
+
+const client: ApolloClient<any> = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+export default client;
