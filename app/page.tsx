f253f4016317d019649294f7c1c6adf8ef28d7ed
brunomoyse <moyse94@gmail.com>
2023-08-27 13:11:51 +0200

    Set it

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-
-import { Kaushan_Script } from "next/font/google";
-const kaushanScript = Kaushan_Script({ weight: '400', subsets: ['latin'] })
-import Image from 'next/image'
 import React from "react";
-import Link from "next/link";
-import { motion } from "framer-motion";
 import Manekineko from "@/components/Manekineko";
 import DeliveryButton from "@/components/DeliveryButton";
 
@@ -22,17 +16,6 @@ export default function Home() {
 
                       <div className="flex flex-col items-center md:items-start justify-center mb-8">
                           <div className="flex flex-col items-center justify-center">
-                              {/*
-                              <h1 className={`${kaushanScript.className} text-9xl w-full mb-4 text-center`}>Tokyo</h1>
-                              <h2 className="text-6xl flex-1 w-full mb-4 text-center">SUSHI & BAR</h2>
-                              <Image
-                                  src="/images/tsb_logo.jpg"
-                                  alt="Tokyo Sushi Bar logo"
-                                  width={250}
-                                  height={250}
-                                  draggable={false}
-                              />
-                              */}
                               <h1 className="lg:text-9xl text-8xl w-full text-center tracking-tight">TOKYO</h1>
                               <h2 className="lg:text-5xl text-4xl flex-1 w-full -mt-2 mb-8 text-center tracking-widest">SUSHI & BAR</h2>
                               <DeliveryButton />
--- components/Manekineko.tsx
+++ components/Manekineko.tsx
@@ -1,26 +1,51 @@
-'use client';
+"use client";
 
-import {motion} from "framer-motion";
-import React from "react";
-import Image from "next/image";
+import React, { useRef, Suspense } from 'react';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
 
-const Manekineko = () => {
+const Model = () => {
+    const gltf = useLoader(GLTFLoader, '/models/manekineko/scene.gltf');
 
+    // Load textures
+    const blackBaseColor = useLoader(THREE.TextureLoader, '/models/manekineko/maneki_black_baseColor.jpeg');
+    const blackMetallicRoughness = useLoader(THREE.TextureLoader, '/models/manekineko/maneki_black_metallicRoughness.png');
+
+    // Assuming the model's mesh material is named 'manekiMaterial', you can adjust this
+    const mesh = gltf.scene.children.find(child => child.isMesh && child.material.name === 'manekiMaterial');
+    if (mesh) {
+        mesh.material.map = blackBaseColor;
+        mesh.material.metalnessMap = blackMetallicRoughness;
+        mesh.material.needsUpdate = true;
+    }
+
+    const ref = useRef<THREE.Mesh>();
+
+    // This will run on every frame, useful if your model has animations
+    useFrame((state, delta) => {
+        if (ref.current) {
+            // If the model has animations, uncomment the next line
+            // state.clock.getElapsedTime() is the time in seconds since the clock started, it can be used to control the animation mixer
+            // gltf.animations.forEach(clip => state.mixers[clip.name].update(delta));
+        }
+    });
+
+    return <primitive ref={ref} object={gltf.scene} />;
+};
+
+const Manekineko: React.FC = () => {
     return (
-        <motion.div
-            initial={({ x: -350 })}
-            animate={{ x: 0 }}
-            transition={{ ease: "easeOut", duration: 0.8 }}
-        >
-            <Image
-                src="/images/maneki-neko-3.png"
-                alt="Tokyo Sushi Bar maneki neko"
-                width={500}
-                height={500}
-                draggable={false}
-            />
-        </motion.div>
+        <Canvas style={{ width: '500px', height: '500px' }}>
+            <Suspense fallback={null}>
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 10]} />
+                <Model />
+            </Suspense>
+            <OrbitControls enablePan={false} />
+        </Canvas>
     );
-}
+};
 
 export default Manekineko;
