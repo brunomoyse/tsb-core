07854fd34f880c19688ca9e7ac7f21e804b19e90
brunomoyse <moyse94@gmail.com>
2023-08-29 15:56:50 +0200

    Push

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,46 @@
+"use client";
 
-import { Kaushan_Script } from "next/font/google";
-const kaushanScript = Kaushan_Script({ weight: '400', subsets: ['latin'] })
-import Image from 'next/image'
-import React from "react";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import Manekineko from "@/components/Manekineko";
-import DeliveryButton from "@/components/DeliveryButton";
+"use client";
 
-export default function Home() {
-  return (
-      <main>
-          <section className="min-h-screen bg-white">
-              <div className="min-h-screen flex justify-center items-center">
-                  <div className="w-full grid md:grid-cols-2 2xl:gap-x-36 xl:gap-x-20 lg:gap-x-12">
+import React, { useEffect } from 'react';
 
-                      <div className="w-full flex flex-col items-center md:items-end justify-center mb-8">
-                          <Manekineko />
-                      </div>
+const Home: React.FC = () => {
+    useEffect(() => {
+        const parallaxLayers = Array.from(document.querySelectorAll('.parallax-layer'));
+        const contentHeight = document.querySelector('.content').clientHeight;
+        const viewportHeight = window.innerHeight;
+        const maxScroll = contentHeight - viewportHeight;
 
+        const handleScroll = () => {
+            const scrolled = window.scrollY;
+            parallaxLayers.forEach((layer: Element) => {
+                // @ts-ignore
+                const speed = parseFloat(layer.getAttribute('data-speed'));
+                let yPos;
+                yPos = -(scrolled * speed * maxScroll / 100);
+                layer.style.backgroundPositionY = `${yPos}px`;
+            });
+        };
 
-                      <div className="flex flex-col items-center md:items-start justify-center mb-8">
-                          <div className="flex flex-col items-center justify-center">
-                              {/*
-                              <h1 className={`${kaushanScript.className} text-9xl w-full mb-4 text-center`}>Tokyo</h1>
-                              <h2 className="text-6xl flex-1 w-full mb-4 text-center">SUSHI & BAR</h2>
-                              <Image
-                                  src="/images/tsb_logo.jpg"
-                                  alt="Tokyo Sushi Bar logo"
-                                  width={250}
-                                  height={250}
-                                  draggable={false}
-                              />
-                              */}
-                              <h1 className="lg:text-9xl text-8xl w-full text-center tracking-tight">TOKYO</h1>
-                              <h2 className="lg:text-5xl text-4xl flex-1 w-full -mt-2 mb-8 text-center tracking-widest">SUSHI & BAR</h2>
-                              <DeliveryButton />
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-      </main>
-  )
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className="parallax">
+            <div className="parallax-layer" data-speed="0.6" style={{ backgroundImage: 'url(/images/parallax/sky.webp)' }}></div>
+            <div className="parallax-layer" data-speed="0.1" style={{ backgroundImage: 'url(/images/parallax/water.webp)' }}></div>
+            <div className="parallax-layer" data-speed="0.4" style={{ backgroundImage: 'url(/images/parallax/trees.webp)' }}></div>
+            <div className="parallax-layer" data-speed="0.1" style={{ backgroundImage: 'url(/images/parallax/rock.webp)' }}></div>
+            <div className="content">
+                <div style={{ minHeight: '150vh' }}>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default Home;
