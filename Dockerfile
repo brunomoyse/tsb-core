b71b518f8883164a4958c93a960ee6dd0b80cec5
brunomoyse <bruno.moyse@nccn.fgov.be>
2023-09-10 11:48:06 +0200

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ FROM node:18.17-alpine
 # Set the working directory
 WORKDIR /usr/src/app
 
+# Create www-data user and group
+#RUN addgroup -S www-data && adduser -S www-data -G www-data
+
 # Install dependencies
 COPY package.json package-lock.json ./
 RUN npm install
@@ -13,6 +16,9 @@ RUN npm install
 # Copy the application code
 COPY . .
 
+RUN mkdir public/images/compressed
+
+RUN node utils/sharp.js
+
 #  Build the Next.js app & start the app
 CMD sh -c "npm run build && npm start"
-
--- utils/sharp.js
+++ utils/sharp.js
@@ -1,35 +1,46 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
-const output = "/Users/brunomoyse/Development/applications/PERSO/tsb/tsb-core/public/images/compressed/";
-const input = "/Users/brunomoyse/Development/applications/PERSO/tsb/tsb-core/public/images/menu/menu/";
+const output = "./public/images/compressed/";
+//let input = "./public/images/menu/";
 const pngQuality = { quality: 80 };
 const webpQuality = { quality: 80, reductionEffort: 6 };
 const avifQuality = { quality: 80, speed: 1 };
 const sizes = [200];
-fs.readdir(input, (err, files) => {
-    console.log(files);
-    console.log("Found " + files.length + " files. Converting now, please be patient..");
-    files.forEach((file) => {
-        function convert(size) {
-            let fileShort = path.parse(file).name;
-            sharp(input + file)
-                .png(pngQuality)
-                .resize({ width: size })
-                .toFile(output + fileShort + "-" + size + ".png");
-            sharp(input + file)
-                .webp(webpQuality)
-                .resize({ width: size })
-                .toFile(output + fileShort + "-" + size + ".webp");
-            sharp(input + file)
-                .avif(avifQuality)
-                .resize({ width: size })
-                .toFile(output + fileShort + "-" + size + ".avif");
-        }
-        if (file.endsWith(".png") || file.endsWith(".jpg") || file.endsWith(".jpeg")) {
-            for (let i = 0; i < sizes.length; i++) {
-                convert(sizes[i]);
+
+const productCategories = [
+    'menu',
+    'sushi',
+];
+
+productCategories.forEach((category) => {
+    let input = "./public/images/menu/" + category + "/";
+
+    fs.readdir(input, (err, files) => {
+        console.log(files);
+        console.log("Found " + files.length + " files. Converting now, please be patient..");
+        files.forEach((file) => {
+            function convert(size) {
+                let fileShort = path.parse(file).name;
+                sharp(input + file)
+                    .png(pngQuality)
+                    .resize({ width: size })
+                    .toFile(output + fileShort + "-" + size + ".png");
+                sharp(input + file)
+                    .webp(webpQuality)
+                    .resize({ width: size })
+                    .toFile(output + fileShort + "-" + size + ".webp");
+                sharp(input + file)
+                    .avif(avifQuality)
+                    .resize({ width: size })
+                    .toFile(output + fileShort + "-" + size + ".avif");
             }
-        }
+            if (file.endsWith(".png") || file.endsWith(".jpg") || file.endsWith(".jpeg")) {
+                for (let i = 0; i < sizes.length; i++) {
+                    convert(sizes[i]);
+                }
+            }
+        });
     });
-});
+})
+
