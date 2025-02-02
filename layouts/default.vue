24d690cf999b29aa710105903c54a2e0f2aeae53
Bruno Moyse <cloud@nuagemagique.dev>
2025-02-02 22:53:43 +0100

    Add html lang attribute

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    <header>
-      <Navbar />
-      <!-- Spacer to prevent content from being hidden behind the fixed Navbar -->
-      <div class="h-20"></div>
-    </header>
-    <main>
-      <slot />
-    </main>
-    <footer></footer>
+    <Html :lang="head.htmlAttrs?.lang ?? 'en'" :dir="head.htmlAttrs?.dir ?? 'ltr'">
+
+    <Head>
+      <Title>{{ title }}</Title>
+      <template v-for="link in head.link" :key="link.hid">
+        <Link :id="link.hid" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+      </template>
+      <template v-for="meta in head.meta" :key="meta.hid">
+        <Meta :id="meta.hid" :property="meta.property" :content="meta.content" />
+      </template>
+    </Head>
+
+    <Body>
+      <header>
+        <Navbar />
+        <!-- Spacer to prevent content from being hidden behind the fixed Navbar -->
+        <div class="h-20"></div>
+      </header>
+      <main>
+        <slot />
+      </main>
+      <footer></footer>
+    </Body>
+
+    </Html>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const route = useRoute()
+const { t } = useI18n()
+const head = useLocaleHead()
+const title = computed(() => t(typeof route.meta.title === 'string' ? route.meta.title : 'Tokyo Sushi Bar'));
 </script>
-
-<style>
-
-</style>
