b1e0928a14e1e9e5fdf1a320187580101c8ac9aa
Bruno Moyse <cloud@nuagemagique.dev>
2025-02-22 20:12:08 +0100

    Fix checkbox animation

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,107 +1,16 @@
 <template>
-    <label class="inline-flex checkbox path">
-        <input type="checkbox">
-        <svg viewBox="0 0 21 21">
-            <path
-                d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186">
-            </path>
+    <label class="flex items-center space-x-2 cursor-pointer">
+      <input type="checkbox" class="hidden peer" v-model="checked">
+      <div class="w-5 h-5 border-2 border-tsb-red-lighter rounded-md flex items-center justify-center peer-checked:bg-tsb-red-lighter peer-checked:border-tsb-red-lighter transition">
+        <svg v-if="checked" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="3">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
         </svg>
-        <span class="ml-2">
-            <slot></slot>
-        </span>
+      </div>
+      <span class="text-gray-700"><slot></slot></span>
     </label>
-</template>
-
-<style>
-/* Checkboxes */
-.checkbox {
-    --background: #fff;
-    --border: #D1D6EE;
-    --border-hover: #BBC1E1;
-    --border-active: #1E2235;
-    --tick: #fff;
-    position: relative;
-
-    input,
-    svg {
-        width: 21px;
-        height: 21px;
-        display: block;
-    }
-
-    input {
-        position: relative;
-        outline: none;
-        background: var(--background);
-        border: none;
-        margin: 0;
-        padding: 0;
-        cursor: pointer;
-        border-radius: 4px;
-        transition: box-shadow .3s;
-        box-shadow: inset 0 0 0 var(--s, 1px) var(--b, var(--border));
-
-        &:hover {
-            --s: 2px;
-            --b: var(--border-hover);
-        }
-
-        &:checked {
-            --b: var(--border-active);
-        }
-    }
-
-    svg {
-        pointer-events: none;
-        fill: none;
-        stroke-width: 2px;
-        stroke-linecap: round;
-        stroke-linejoin: round;
-        stroke: var(--stroke, var(--border-active));
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 21px;
-        height: 21px;
-        transform: scale(var(--scale, 1)) translateZ(0);
-    }
-
-    &.path {
-        input {
-            &:checked {
-                --s: 2px;
-                transition-delay: .4s;
-
-                &+svg {
-                    --a: 16.1 86.12;
-                    --o: 102.22;
-                }
-            }
-        }
-
-        svg {
-            stroke-dasharray: var(--a, 86.12);
-            stroke-dashoffset: var(--o, 86.12);
-            transition: stroke-dasharray .6s, stroke-dashoffset .6s;
-        }
-    }
-
-    &.bounce {
-        --stroke: var(--tick);
-
-        input {
-            &:checked {
-                --s: 11px;
-
-                &+svg {
-                    animation: bounce .4s linear forwards .2s;
-                }
-            }
-        }
-
-        svg {
-            --scale: 0;
-        }
-    }
-}
-</style>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  const checked = ref(false);
+  </script>
