e704d83ad34e0bd7b27b74072223b13ed5fe4fbf
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2025-02-24 11:18:48 +0100

    Fix tooltips

--- components/LanguagePicker.vue
+++ components/LanguagePicker.vue
@@ -5,13 +5,13 @@
             class="relative group w-[50px] h-[50px] bg-white flex items-center justify-center rounded-full hover:shadow-md transition-shadow overflow-visible cursor-pointer">
             <img :src="icon" :alt="alt" class="h-6 w-6" />
             <span
-                class="absolute left-1/2 top-full -translate-x-1/2 mt-2 px-2 py-1 bg-black text-white text-xs rounded opacity-0 group-hover:opacity-100 transition whitespace-nowrap z-50">
+                class="absolute left-1/2 top-full -translate-x-1/2 mt-2 px-2 py-1 bg-black text-white text-xs rounded opacity-0 group-hover:opacity-100 transition whitespace-nowrap z-50 min-w-max">
                 {{ tooltipText }}
             </span>
         </div>
         <!-- Dropdown Menu: appears to the right of the icon -->
         <ul v-if="showDropdown"
-            class="absolute left-full bottom-0 ml-2 bg-white border border-gray-200 rounded shadow-lg z-50">
+            class="absolute left-full bottom-0 ml-2 bg-white border border-gray-200 rounded shadow-lg z-50 min-w-max">
             <li v-for="loc in localesToPick" :key="loc.code" class="whitespace-nowrap">
                 <NuxtLink :to="switchLocalePath(loc.code)" class="block px-4 py-2 hover:bg-gray-100"
                     @click="hideDropdown">
