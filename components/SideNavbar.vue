26b8242caedbda77312114d7c178125272b8db80
Bruno Moyse <cloud@nuagemagique.dev>
2025-02-24 12:21:04 +0100

    App nav item isActive

--- components/SideNavbar.vue
+++ components/SideNavbar.vue
@@ -6,15 +6,15 @@
     <!-- Top Navigation Items -->
     <ul class="flex flex-col items-center space-y-6 mt-6">
       <Logo to="/" icon="/icons/tsb-logo.svg" alt="Tokyo Sushi Bar logo" :tooltipText="$t('nav.home')" />
-      <NavItem to="/menu" icon="/icons/menu-icon.svg" alt="Menu Icon" :tooltipText="$t('nav.menu')" />
-      <NavItem to="/reservation" icon="/icons/reservation-icon.svg" alt="Reservation Icon" :tooltipText="$t('nav.book')" />
-      <NavItem to="/contact" icon="/icons/contact-icon.svg" alt="Contact Icon" :tooltipText="$t('nav.contact')" />
+      <NavItem to="menu" icon="/icons/menu-icon.svg" alt="Menu Icon" :tooltipText="$t('nav.menu')" />
+      <NavItem to="reservation" icon="/icons/reservation-icon.svg" alt="Reservation Icon" :tooltipText="$t('nav.book')" />
+      <NavItem to="contact" icon="/icons/contact-icon.svg" alt="Contact Icon" :tooltipText="$t('nav.contact')" />
     </ul>
 
     <!-- Bottom Navigation Items -->
     <ul class="flex flex-col items-center space-y-6 mb-6">
-      <NavItem v-if="!isUserConnected" to="/login" icon="/icons/login-icon.svg" alt="Login Icon" :tooltipText="$t('nav.login')" />
-      <NavItem v-if="isUserConnected" to="/logout" icon="/icons/logout-icon.svg" alt="Logout Icon" :tooltipText="$t('nav.logout')" />
+      <NavItem v-if="!isUserConnected" to="login" icon="/icons/login-icon.svg" alt="Login Icon" :tooltipText="$t('nav.login')" />
+      <NavItem v-if="isUserConnected" to="logout" icon="/icons/logout-icon.svg" alt="Logout Icon" :tooltipText="$t('nav.logout')" />
       <!-- Language picker -->
       <LanguagePicker icon="/icons/translate-icon.svg" alt="Choose language Icon" :tooltipText="$t('nav.language')" />
     </ul>
