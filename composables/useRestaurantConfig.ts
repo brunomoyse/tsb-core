5c862b03370a24fc49a18e6a0a5a91a6de173821
Bruno Moyse <cloud@nuagemagique.dev>
2026-02-23 21:49:57 +0100

    Add real-time subscriptions for restaurant config and product updates

--- composables/useRestaurantConfig.ts
+++ composables/useRestaurantConfig.ts
@@ -1,9 +1,22 @@
+import { watch } from 'vue'
 import gql from 'graphql-tag'
+import { print } from 'graphql'
 import { useGqlQuery } from './useGqlQuery'
+import { useGqlSubscription } from './useGqlSubscription'
 
 const RESTAURANT_CONFIG_QUERY = gql`
     query RestaurantConfig {
         restaurantConfig {
+            orderingEnabled
+            openingHours
+            isCurrentlyOpen
+        }
+    }
+`
+
+const SUB_RESTAURANT_CONFIG = gql`
+    subscription RestaurantConfigUpdated {
+        restaurantConfigUpdated {
             orderingEnabled
             openingHours
             isCurrentlyOpen
@@ -23,6 +36,23 @@ export async function useRestaurantConfig() {
         {},
         { immediate: true, cache: false }
     )
+
+    if (import.meta.client) {
+        const { data: liveConfig } = useGqlSubscription<{ restaurantConfigUpdated: RestaurantConfig }>(
+            print(SUB_RESTAURANT_CONFIG)
+        )
+        watch(liveConfig, (val) => {
+            if (val?.restaurantConfigUpdated && data.value) {
+                data.value = {
+                    ...data.value,
+                    restaurantConfig: {
+                        ...data.value.restaurantConfig,
+                        ...val.restaurantConfigUpdated,
+                    },
+                }
+            }
+        })
+    }
 
     return {
         config: data,
