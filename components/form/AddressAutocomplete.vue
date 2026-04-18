b137f2420ede45a073dfe125fdbd58d59dfcbbe4
Bruno Moyse <cloud@nuagemagique.dev>
2026-04-18 11:32:38 +0200

    fix: improve ux on address completion

--- components/form/AddressAutocomplete.vue
+++ components/form/AddressAutocomplete.vue
@@ -60,10 +60,15 @@
                 <svg class="w-4 h-4 shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01M10.29 3.86L1.82 18a2 2 0 001.71 3h16.94a2 2 0 001.71-3L13.71 3.86a2 2 0 00-3.42 0z" />
                 </svg>
-                <p>
-                    <span class="font-semibold">
-                        {{ showNoMatchHint ? $t('register.addressNoMatch') : $t('register.addressHouseNumberRequired') }}
+                <p v-if="showNoMatchHint">
+                    <span class="font-semibold">{{ $t('register.addressNoMatch') }}</span>
+                    <span class="block mt-0.5 opacity-90">
+                        {{ $t('register.addressCallHint') }}
+                        <a href="tel:+32422298888" class="underline font-medium whitespace-nowrap">+32 4 222 98 88</a>
                     </span>
+                </p>
+                <p v-else>
+                    <span class="font-semibold">{{ $t('register.addressHouseNumberRequired') }}</span>
                     <span class="block mt-0.5 opacity-90">{{ $t('register.addressExample') }}</span>
                 </p>
             </div>
